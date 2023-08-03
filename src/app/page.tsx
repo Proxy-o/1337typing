42fbb09a27bdea927de066610d4313a861e7b729
PROXY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-08-03 17:16:42 +0100

    [REF] move the text component to  a separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Text from "@/components/text.component";
 import useKeyPress from "@/hooks/useKeyPress";
 import { useState, useEffect, useRef, use } from "react";
 
@@ -176,44 +177,13 @@ export default function Home() {
 		}
 	});
 
-	return (
-		<div className="bg-gray-800 flex  h-screen justify-center items-center  text-gray-300 flex-col">
-			<div className="flex flex-col  items-center w-full">
-				{countDown > 0 && countDown}
-			</div>
-			{countDown === 0 && <div className="flex">{wpm} WPM</div>}
-			{countDown === 0 && <div className="flex">{accuracy}%</div>}
-			<div className=" flex">
-				{wordsArr.map((word, word_index) => (
-					<div key={word_index} className=" mr-2 ">
-						{word.split("").map((letter, index) => (
-							<>
-								<span
-									key={index}
-									className={`${
-										words[word_index][index].style === "correct"
-											? "text-gray-300"
-											: words[word_index][index].style === "incorrect"
-											? "text-red-500 "
-											: "text-slate-500"
-									}`}
-								>
-									<span
-										className={`${
-											word_index === currentWordIndex &&
-											index === currentLetterIndex
-												? " text-base border-b-2 border-yellow-600  "
-												: ""
-										}`}
-									>
-										{letter}
-									</span>
-								</span>
-							</>
-						))}
-					</div>
-				))}
-			</div>
-		</div>
-	);
+	return Text({
+		countDown,
+		wpm,
+		accuracy,
+		wordsArr,
+		currentWordIndex,
+		currentLetterIndex,
+		words,
+	});
 }
---
+++ src/components/text.component.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function Text({
+	countDown,
+	wpm,
+	accuracy,
+	wordsArr,
+	currentWordIndex,
+	currentLetterIndex,
+	words,
+}: {
+	countDown: number;
+	wpm: number;
+	accuracy: number;
+	wordsArr: string[];
+	currentWordIndex: number;
+	currentLetterIndex: number;
+	words: any;
+}) {
+	return (
+		<div className="bg-gray-800 flex  h-screen justify-center items-center  text-gray-300 flex-col">
+			<div className="flex flex-col  items-center w-full">
+				{countDown > 0 && countDown}
+			</div>
+			{countDown === 0 && <div className="flex">{wpm} WPM</div>}
+			{countDown === 0 && <div className="flex">{accuracy}%</div>}
+			<div className=" flex">
+				{wordsArr.map((word, word_index) => (
+					<div key={word_index} className=" mr-2 ">
+						{word.split("").map((letter, index) => (
+							<>
+								<span
+									key={index}
+									className={`${
+										words[word_index][index].style === "correct"
+											? "text-gray-300"
+											: words[word_index][index].style === "incorrect"
+											? "text-red-500 "
+											: "text-slate-500"
+									}`}
+								>
+									<span
+										className={`${
+											word_index === currentWordIndex &&
+											index === currentLetterIndex
+												? " text-base border-b-2 border-yellow-600  "
+												: ""
+										}`}
+									>
+										{letter}
+									</span>
+								</span>
+							</>
+						))}
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Text;
